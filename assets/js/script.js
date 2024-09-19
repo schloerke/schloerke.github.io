fbe51777f8271645b060c45e6ad8d697ebd8c6f0
Barret Schloerke <barret@posit.co>
2024-09-19 13:07:57 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,17 +150,17 @@ function scroll() {
   // Testimonial Slide
   ///////////////////////////////
 
- $(document).ready(function() {
-
-  $("#testimonial-container").owlCarousel({
-
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 700,
-      paginationSpeed : 400,
-      singleItem:true,
-  });
-
-});
+//  $(document).ready(function() {
+// 
+//   $("#testimonial-container").owlCarousel({
+// 
+//       navigation : false, // Show next and prev buttons
+//       slideSpeed : 700,
+//       paginationSpeed : 400,
+//       singleItem:true,
+//   });
+// 
+// });
 
 
   ///////////////////////////////
